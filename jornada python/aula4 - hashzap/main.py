d400cb0f5fbb14fd89d22a092c91794052071ff9
Edigar Maia <edigarmaia@gmail.com>
2024-03-07 14:25:33 -0300

    Projeto finalizado

--- jornada python/aula4 - hashzap/main.py
+++ jornada python/aula4 - hashzap/main.py
@@ -13,16 +13,28 @@
 
 # importar o flask
 from flask import Flask, render_template
+from flask_socketio import SocketIO, send
+
 
 # criar o app
 app = Flask(__name__)
+socketio = SocketIO(app, cors_allowed_origin="*")
+
+
+# funcionalidade de enviar mensagem
+@socketio.on("message")
+def gerenciar_mensagem(mensagem):
+    send(mensagem, broadcast=True)
+
 
 # criar a primeira pagina - primeira rota
 @app.route("/")
 def homepage():
-    return render_template("homepage.html")
+    return render_template("index.html")
+
 
 # roda o aplicativo
-app.run()
+socketio.run(app, host="192.168.100.153")
 
-# websocket
+# websocket -> faz a conexão entre duas pessoas que estão usando o mesmo site
+# pip install python-socketio flask-socketio simple-websocket
